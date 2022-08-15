0477b5620fbe4d8e8ae93002a531a4feb337ab3b
shudson <shudson@anl.gov>
2022-08-15 17:24:35 -0500

    Update libEnsemble test

--- libensemble/test_persistent_aposmm_tao.py
+++ libensemble/test_persistent_aposmm_tao.py
@@ -1,82 +1,106 @@
-# """
-# Runs libEnsemble on the 6-hump camel problem. Documented here:
-#    https://www.sfu.ca/~ssurjano/camel6.html
-#
-# Execute via one of the following commands (e.g. 3 workers):
-#    mpiexec -np 4 python3 test_persistent_aposmm_tao.py
-#    python3 test_persistent_aposmm_tao.py --nworkers 3 --comms local
-#    python3 test_persistent_aposmm_tao.py --nworkers 3 --comms tcp
-#
-# The number of concurrent evaluations of the objective function will be 4-1=3.
-# """
+"""
+Runs libEnsemble with APOSMM with a PETSc/TAO local optimizer.
+
+Execute via one of the following commands (e.g. 3 workers):
+   mpiexec -np 4 python test_persistent_aposmm_tao_blmvm.py
+   python test_persistent_aposmm_tao_blmvm.py --nworkers 3 --comms local
+   python test_persistent_aposmm_tao_blmvm.py --nworkers 3 --comms tcp
+
+When running with the above commands, the number of concurrent evaluations of
+the objective function will be 2, as one of the three workers will be the
+persistent generator.
+"""
 
 # Do not change these lines - they are parsed by run-tests.sh
-# TESTSUITE_COMMS: local
+# TESTSUITE_COMMS: local mpi tcp
 # TESTSUITE_NPROCS: 4
+# TESTSUITE_EXTRA: true
 
 import sys
+import multiprocessing
 import numpy as np
 
 # Import libEnsemble items for this test
 from libensemble.libE import libE
 from math import gamma, pi, sqrt
 from libensemble.sim_funcs.six_hump_camel import six_hump_camel as sim_f
-from persistent_aposmm import aposmm as gen_f
-from persistent_aposmm_alloc import persistent_aposmm_alloc as alloc_f
-from libensemble.tests.regression_tests.common import parse_args, save_libE_output, per_worker_stream
+
+import libensemble.gen_funcs
+
+libensemble.gen_funcs.rc.aposmm_optimizers = "petsc"
+from libensemble.gen_funcs.persistent_aposmm import aposmm as gen_f
+
+from libensemble.alloc_funcs.persistent_aposmm_alloc import persistent_aposmm_alloc as alloc_f
+from libensemble.tools import parse_args, save_libE_output, add_unique_random_streams
 from libensemble.tests.regression_tests.support import six_hump_camel_minima as minima
 from time import time
 
-nworkers, is_master, libE_specs, _ = parse_args()
+# Main block is necessary only when using local comms with spawn start method (default on macOS and Windows).
+if __name__ == "__main__":
 
-if is_master:
-    start_time = time()
+    # Temporary solution while we investigate/resolve slowdowns with "spawn" start method.
+    multiprocessing.set_start_method("fork", force=True)
 
-if nworkers < 2:
-    sys.exit("Cannot run with a persistent worker if only one worker -- aborting...")
+    nworkers, is_manager, libE_specs, _ = parse_args()
 
-n = 2
-sim_specs = {'sim_f': sim_f,
-             'in': ['x'],
-             'out': [('f', float), ('grad', float, n)]}
+    if is_manager:
+        start_time = time()
 
-gen_out = [('x', float, n), ('x_on_cube', float, n), ('sim_id', int),
-           ('local_min', bool), ('local_pt', bool)]
+    if nworkers < 2:
+        sys.exit("Cannot run with a persistent worker if only one worker -- aborting...")
 
-gen_specs = {'gen_f': gen_f,
-             'in': [],
-             'out': gen_out,
-             'initial_sample_size': 100,
-             'sample_points': np.round(minima, 1),
-             'localopt_method': 'blmvm',
-             'rk_const': 0.5*((gamma(1+(n/2))*5)**(1/n))/sqrt(pi),
-             'grtol': 1e-4,
-             'gatol': 1e-4,
-             'dist_to_bound_multiple': 0.5,
-             'max_active_runs': 6,
-             'lb': np.array([-3, -2]),
-             'ub': np.array([3, 2])
-             }
+    n = 2
+    sim_specs = {
+        "sim_f": sim_f,
+        "in": ["x"],
+        "out": [("f", float), ("grad", float, n)],
+    }
 
-alloc_specs = {'alloc_f': alloc_f, 'out': [('given_back', bool)], 'user': {}}
+    gen_out = [
+        ("x", float, n),
+        ("x_on_cube", float, n),
+        ("sim_id", int),
+        ("local_min", bool),
+        ("local_pt", bool),
+    ]
 
-persis_info = per_worker_stream({}, nworkers + 1)
+    gen_specs = {
+        "gen_f": gen_f,
+        "persis_in": ["f", "grad"] + [n[0] for n in gen_out],
+        "out": gen_out,
+        "user": {
+            "initial_sample_size": 100,
+            "sample_points": np.round(minima, 1),
+            "localopt_method": "blmvm",
+            "rk_const": 0.5 * ((gamma(1 + (n / 2)) * 5) ** (1 / n)) / sqrt(pi),
+            "grtol": 1e-4,
+            "gatol": 1e-4,
+            "dist_to_bound_multiple": 0.5,
+            "max_active_runs": 6,
+            "lb": np.array([-3, -2]),
+            "ub": np.array([3, 2]),
+        },
+    }
 
-exit_criteria = {'sim_max': 1000}
+    alloc_specs = {"alloc_f": alloc_f}
 
-# Perform the run
-H, persis_info, flag = libE(sim_specs, gen_specs, exit_criteria, persis_info,
-                            alloc_specs, libE_specs)
+    persis_info = add_unique_random_streams({}, nworkers + 1)
 
-if is_master:
-    print('[Manager]:', H[np.where(H['local_min'])]['x'])
-    print('[Manager]: Time taken =', time() - start_time, flush=True)
+    exit_criteria = {"sim_max": 1000}
 
-    tol = 1e-5
-    for m in minima:
-        # The minima are known on this test problem.
-        # We use their values to test APOSMM has identified all minima
-        print(np.min(np.sum((H[H['local_min']]['x'] - m)**2, 1)), flush=True)
-        assert np.min(np.sum((H[H['local_min']]['x'] - m)**2, 1)) < tol
+    # Perform the run
+    H, persis_info, flag = libE(sim_specs, gen_specs, exit_criteria, persis_info, alloc_specs, libE_specs)
 
-    save_libE_output(H, persis_info, __file__, nworkers)
+    if is_manager:
+        print("[Manager]:", H[np.where(H["local_min"])]["x"])
+        print("[Manager]: Time taken =", time() - start_time, flush=True)
+
+        tol = 1e-5
+        for m in minima:
+            # The minima are known on this test problem.
+            # We use their values to test APOSMM has identified all minima
+            print(np.min(np.sum((H[H["local_min"]]["x"] - m) ** 2, 1)), flush=True)
+            assert np.min(np.sum((H[H["local_min"]]["x"] - m) ** 2, 1)) < tol
+
+        save_libE_output(H, persis_info, __file__, nworkers)
+
